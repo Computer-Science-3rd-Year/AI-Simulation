c03a4e87977c36b211ed87c9f701196a16f48705
NinaSayers <massypaz2001git@gmail.com>
2024-06-22 14:13:12 -0400

    base_simulation_to_growup

---
+++ project/Agents/manager.py
@@ -0,0 +1,48 @@
+# class Manager:
+#     def __init__(self, hotel):
+#         self.hotel = hotel
+#         self.estrategia = {
+#             "atracciones": [],
+#             "servicios": [],
+#             "personal": [],
+#         }
+#         self.metas = {
+#             "ingresos": 0,
+#             "satisfaccion_turistas": 0,
+#             "reputacion": 0,
+#         }
+
+#     def evaluar_resultados(self):
+#         """
+#         Evalúa la situación actual del hotel.
+#         """
+#         ingresos = self.hotel.calcular_ingresos()
+#         satisfaccion = self.hotel.calcular_satisfaccion_turistas()
+#         reputacion = self.hotel.calcular_reputacion()
+#         self.metas["ingresos"] = ingresos
+#         self.metas["satisfaccion_turistas"] = satisfaccion
+#         self.metas["reputacion"] = reputacion
+
+#     def tomar_decisiones(self):
+#         """
+#         Toma decisiones basadas en reglas simples.
+#         """
+#         # Reglas básicas de negocio
+#         if self.metas["ingresos"] < self.hotel.ingresos_objetivo:
+#             if self.metas["reputacion"] < 5:
+#                 self.hotel.agregar_atraccion("Bar")
+#                 self.estrategia["atracciones"].append("Bar")
+#             else:
+#                 self.hotel.contratar_trabajador("Bartender")
+#                 self.estrategia["personal"].append("Bartender")
+#         if self.metas["satisfaccion_turistas"] < 7:
+#             self.hotel.mejorar_servicios()
+#             self.estrategia["servicios"].append("Mejorar calidad")
+
+#     def ejecutar_estrategia(self):
+#         """
+#         Ejecuta la estrategia del Manager
+#         """
+#         self.evaluar_resultados()
+#         self.tomar_decisiones()
+
---
+++ project/Agents/tourists.py
@@ -0,0 +1,18 @@
+# class Tourist(object):
+#     def __init__(self, env, nombre, hotel):
+#         self.env = env
+#         self.nombre = nombre
+#         self.hotel = hotel
+#         self.action = env.process(self.run())
+    
+#     def run(self):
+#         # El turista llega al hotel 
+#         print(f'{self.env.now}: {self.nombre} llega al hotel')
+#         # El turista reserva una habitación
+#         with self.hotel.habitaciones.request() as req:
+#             yield req
+#             print(f'{self.env.now}: {self.nombre} se registra en la habitación')
+#             yield self.env.timeout(2)  # Tiempo que el turista pasa en la habitación 
+#             print(f'{self.env.now}: {self.nombre} sale del hotel') 
+
+#         #tiempos_salida.append(self.env.now)  # Guarda el tiempo de salida del turista
